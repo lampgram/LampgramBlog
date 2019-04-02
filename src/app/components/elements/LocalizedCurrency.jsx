4bd18448936633e8b8f3c10e1b6a00fe80b2b372
Kate <k.karpo.a@gmail.com>
2019-04-02 15:13:09 +0300

    Create LocalizedCurrency.jsx

---
+++ src/app/components/elements/LocalizedCurrency.jsx
@@ -0,0 +1,6 @@
+import {DEFAULT_CURRENCY} from '../../client_config';
+
+let localCurrencySymbol = DEFAULT_CURRENCY;
+let localizedCurrency = (value) => value;
+
+export {localizedCurrency, localCurrencySymbol}
