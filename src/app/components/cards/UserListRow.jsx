17f56f5ad1307f40f74ef852dbf607476d8dd70e
Kate <k.karpo.a@gmail.com>
2019-04-02 15:14:26 +0300

    Create UserListRow.jsx

---
+++ src/app/components/cards/UserListRow.jsx
@@ -0,0 +1,30 @@
+import React from 'react';
+import {Link} from 'react-router';
+import Follow from '../elements/Follow';
+import {connect} from 'react-redux'
+
+class UserListRow extends React.Component {
+  render() {
+    const {user, loggedIn} = this.props
+    return (
+      <tr>
+        {loggedIn && <td width="250">
+          <Follow following={user}/>
+        </td>}
+        <td>
+          <Link to={'/@' + user}><strong>{user}</strong></Link>
+        </td>
+      </tr>
+    );
+  }
+}
+
+export default connect(
+  (state, ownProps) => {
+    const loggedIn = state.user.hasIn(['current', 'username'])
+    return {
+      ...ownProps,
+      loggedIn
+    }
+  },
+)(UserListRow)
