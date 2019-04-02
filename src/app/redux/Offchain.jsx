edfa34ddaca540c64727fe893390996c7e3c269c
Kate <k.karpo.a@gmail.com>
2019-04-02 15:06:34 +0300

    Create Offchain.jsx

---
+++ src/app/redux/Offchain.jsx
@@ -0,0 +1,12 @@
+import Immutable from 'immutable';
+
+const defaultState = Immutable.fromJS({user: {}});
+
+export default function reducer(state = defaultState, action) {
+  if (action.type === 'user/SAVE_LOGIN_CONFIRM') {
+    if (!action.payload) {
+      state = state.set('account', null);
+    }
+  }
+  return state;
+}
